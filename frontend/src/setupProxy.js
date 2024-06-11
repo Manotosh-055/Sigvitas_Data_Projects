46e2b9b17e61c37b6f35d6c4a76be29962d7c1ab
Manotosh-055 <manotoshkarmakar20512@gmail.com>
2024-06-11 15:04:08 +0530

    change in setupproxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://sigvitas-data-projects.vercel.app',
       changeOrigin: true,
     })
   );
